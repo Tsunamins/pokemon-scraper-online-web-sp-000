cb674d393174f5df43fc2414bce1e49c235e2c2a
Alison Reilly <reillyamr@gmail.com>
2019-02-17 01:00:47 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,7 @@ class Pokemon
   
   def self.find(id, db)
     find_db = db.execute("SELECT pokemon.id, pokemon.name, pokemon.type FROM pokemon WHERE pokemon.id = ?", id).flatten
-    #binding.pry
+    
     
     
     
@@ -30,7 +30,7 @@ class Pokemon
     @type = find_db[2]
     @db = db
     
-    #add_pokemon = self.new
+    
     
     
      add_pokemon = self.new(id: find_db[0], name: find_db[1], type: find_db[2], db: db)
